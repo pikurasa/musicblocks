0cf11edbdd45f5d6a3da52ab029a93a9375b1f03
Devin Ulibarri <devin@devinulibarri.com>
2018-10-25 11:09:08 -0400

    Adjust pitch of banjo down an octave, consistent with others

--- js/utils/synthutils.js
+++ js/utils/synthutils.js
@@ -147,7 +147,7 @@ const SAMPLECENTERNO = {
     'clarinet': ['C4', 39], // pitchToNumber('C', 4, 'C Major')],
     'tuba': ['A#4', 49], // pitchToNumber('A#', 4, 'C Major')],
     'trumpet': ['C3', 27], // pitchToNumber('C', 3, 'C Major')],
-    'banjo': ['C5', 51],
+    'banjo': ['C6', 63],
     'koto': ['C5', 51]
 };
 
